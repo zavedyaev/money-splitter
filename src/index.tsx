6cd4be6d72ae6b414cb8978601734c5f1ee68d2e
Vladimir Zavedyaev <zavedyaev@gmail.com>
2020-09-20 23:29:04 +0700

    Added react support

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import 'bootstrap';
+import './scss/custom.scss';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
